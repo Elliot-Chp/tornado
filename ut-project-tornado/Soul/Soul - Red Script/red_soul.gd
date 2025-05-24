4d331ff1f6a6f0880c8a67aa235d1922194c0eb9
Elliot-Chp <over27percentelliot@gmail.com>
2025-05-23 21:35:32 -0400

    Update red_soul.gd

--- ut-project-tornado/Soul/Soul - Red Script/red_soul.gd
+++ ut-project-tornado/Soul/Soul - Red Script/red_soul.gd
@@ -3,20 +3,22 @@ extends CharacterBody2D
 @export var SoulSpeed = 200
 @export var IsBlue = false
 @export var IsParry = false
-@export var gravity = 2
+@export var gravity = 10
 var BlueSoulJump = 200
-var BlueSoulGravity = 200 
-@export var velo_timer : Timer
-
 
 const terminal : float = 650.0
+
+@export var jumpTime = 0.5
+var jumpRemaining = jumpTime
+
+var feet = true
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
 
-func get_input(_delta):
+func get_input(delta):
 	var dir = Input.get_vector("Left", "Right", "Up", "Down")
 	var BlueSoulDir = Input.get_axis("Left", "Right")
 	if IsBlue == false:
@@ -24,46 +26,33 @@ func get_input(_delta):
 		
 	else: 
 		velocity.x = BlueSoulDir * SoulSpeed
-		if (Input.is_action_pressed("Up") and is_on_floor()) and velocity.y <= 0:
-			velocity.y -= BlueSoulJump
-		if Input.is_action_just_released("Up") and velocity.y <= 0:
-			# gradually increase y velocity
-			for i in range(60):
-				velocity.y += (BlueSoulGravity / 60)
-				# wait for 1000/60 ms
-				velo_timer.start()
-	
-	
+		if (Input.is_action_pressed("Up")) and jumpRemaining > 0 and feet:
+			velocity.y = -BlueSoulJump
+			jumpRemaining -= delta
+			print(jumpRemaining)
+			
+		if (Input.is_action_just_released("Up")):
+			feet = false
 	
 	if Input.is_action_pressed("SlowDown"):
 		if IsBlue == false:
 			velocity = velocity /2
 		pass
-		
-	
-
-		
-	
-	
-	
-	
-
-
-
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
 	get_input(delta)
-	move_and_slide()
 	
-	# If is in the air, gravity is applied
-	if !is_on_floor() and IsBlue == true:
-		velocity.y += gravity
+	if is_on_floor():
+		jumpRemaining = jumpTime
+		feet = true
+	
+	if IsBlue:
+		velocity.y += gravity 
 		if velocity.y > terminal:
 			velocity.y = terminal
 
-	pass
+	move_and_slide()
 
 
 func _on_timer_timeout() -> void:
